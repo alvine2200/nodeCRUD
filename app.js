f71d99eff85bd73c0e612acc2ec2b3832afcf005
alvine2200 <alvinellavu@gmail.com>
2023-01-11 17:02:55 +0300

    db connection

--- app.js
+++ app.js
@@ -2,12 +2,26 @@ const express = require("express");
 
 const exphbs = require("express-handlebars");
 const bodyParser = require("body-parser");
-const mysql = require("mysql");
+const mysql = require("mysql2");
 
 require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+// database credentials
+const pool = mysql.createPool({
+  connectionLimit: 100,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+});
+
+pool.getConnection((err, conn) => {
+  if (err) throw err;
+  console.log("connected as id" + conn.threadId);
+});
 
 // parsing midleware
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -19,7 +33,7 @@ app.use(bodyParser.json());
 app.use(express.static("public"));
 
 // template engines for handlesbars hbs
-app.engine("hbs", exphbs.engine({ extname: ".hbs" }));
+app.engine("hbs", exphbs.engine({ defaultLayout: "main", extname: ".hbs" }));
 app.set("view engine", "hbs");
 
 app.get("", (req, res) => {
