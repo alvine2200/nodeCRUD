1d09f0546b74f72ba88aacab7d9242ab1a0098e3
alvine2200 <alvinellavu@gmail.com>
2023-01-12 10:51:59 +0300

    user added, alerts shown

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -27,3 +27,52 @@ exports.index = (req, res) => {
     });
   });
 };
+
+exports.find = (req, res) => {
+  pool.getConnection((err, conn) => {
+    if (err) throw err;
+    console.log("connected as id" + conn.threadId);
+
+    let searchKey = req.body.search;
+
+    conn.query(
+      "SELECT * FROM users where first_name like ? or last_name like ? or email like ?",
+      ["%" + searchKey + "%", "%" + searchKey + "%", "%" + searchKey + "%"],
+      (err, rows) => {
+        conn.release();
+
+        if (!err) {
+          res.render("home", { rows });
+        } else {
+          console.log(err);
+        }
+
+        console.log(rows);
+      }
+    );
+  });
+};
+
+exports.add_user = (req, res) => {
+  console.log("User Page hit");
+  res.render("add_user");
+};
+
+exports.create_user = (req, res) => {
+  const { first_name, last_name, email, phone } = req.body;
+  pool.getConnection((err, conn) => {
+    if (err) throw err;
+    conn.query(
+      "INSERT INTO users SET first_name = ?, last_name = ? , email = ? , phone = ?",
+      [first_name, last_name, email, phone],
+      (err, rows) => {
+        if (!err) {
+          res.render("add_user", { alert: "User Added Successfully" });
+          console.log("User created successfully with id " + conn.threadId);
+        } else {
+          console.log(err);
+        }
+      }
+    );
+  });
+};
--- server/routes/user.js
+++ server/routes/user.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const userController = require("../controllers/UserController");
 
 router.get("", userController.index);
+router.post("", userController.find);
+router.get("/addUser", userController.add_user);
+router.post("/create_user", userController.create_user);
 
 module.exports = router;
